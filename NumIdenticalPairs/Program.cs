72f87bd98e6bdcb739193db754db65a96ff5297b
PiusAsh <Pius1ash@gmail.com>
2022-08-24 08:40:00 +0100

    Add project files.

---
+++ NumIdenticalPairs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumIdenticalPairs
+{
+
+
+    class Program
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var numsVisited = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+                if (numsVisited.ContainsKey(diff))
+                {
+                    int[] res = new int[] { numsVisited[diff], i };
+                    return res;
+                }
+                else
+                {
+                    numsVisited.Add(nums[i], i);
+                }
+
+            }
+            return new int[] { 0, 0 };
+        }
+        static void Main(string[] args)
+        {
+            int[] res = { 11, 7, 2, 15 };
+            int target = 9;
+            int [] result = (TwoSum(res, target));
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
